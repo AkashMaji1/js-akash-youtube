310d719f5ff360b5a18280f6b7abb3088e988083
Akash Maji <akashmaji6092@gmail.com>
2026-05-02 02:42:44 +0530

    Uncomment comparison examples in comparision.js
    Uncomment comparison console logs to demonstrate behavior.

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -8,10 +8,10 @@
 
 */
 
-// console.log("2" > 1); //true
-// console.log("02" > 1); //true
-// console.log("2" > "12"); //true
-// console.log("2" > "3"); //false
+console.log("2" > 1); //true
+console.log("02" > 1); //true
+console.log("2" > "12"); //true
+console.log("2" > "3"); //false
 
 console.log(null > 0); //false (null convert into 0)
 console.log(null == 0); //false
